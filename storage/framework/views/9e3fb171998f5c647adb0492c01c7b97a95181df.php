ca78c375f6600d383af246a0582a7153cb3be254
Ali Yılmaz <aliyilmaz.work@gmail.com>
2022-07-14 16:02:41 +0300

    Add files via upload

---
+++ storage/framework/views/9e3fb171998f5c647adb0492c01c7b97a95181df.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 9 CRUD Opration - Nicesnippest.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+
+</body>
+</html>
+<?php /**PATH /Users/clickianurgul/Desktop/laravel-9-crud-example/resources/views/post/layout.blade.php ENDPATH**/ ?>
